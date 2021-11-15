c824a4146a3ba5350f87018b6059552b91a08c68
ShimpeiSaito <ne201103@senshu-u.jp>
2021-11-15 09:13:19 +0000

    friendsコントローラのnew,edit,create,update,destroyアクションの作成

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -8,18 +8,36 @@ class FriendsController < ApplicationController
     end
 
     def new
+        @friend = Friend.new
     end
 
     def edit
+        @friend = Friend.find(params[:id])
     end
 
     def create
+        @friend = Friend.new(params[:friend])
+        if @friend.save
+            redirect_to @friend, notice: "友達を登録しました。"
+        else
+            render "new"
+        end
     end
 
     def update
+        @friend = Friend.find(params[:id])
+        @friend.assign_attributes(params[:friend])
+        if @friend.save
+            redirect_to @friend, notice: "友達を更新しました。"
+        else
+            render "edit"
+        end
     end
 
     def destroy
+        @friend = Friend.find(params[:id])
+        @friend.destroy
+        redirect_to :friends, notice: "友達を削除しました。"
     end
 
 end
